9d6b31eae739a840e64e4a79af87a71d570411cb
fehnicchio <fh_nichio@hotmail.com>
2025-02-04 14:15:04 -0300

    subindo soundfy

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './componentes/Header/Header';
+import Sidebar from './componentes/Sidebar/Sidebar';
+import Main from './componentes/Main/Main';
+import Footer from './componentes/Footer/Footer';
+
+
+function App() {
+  return (
+    <div>
+      <Sidebar />
+      <Header />
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
